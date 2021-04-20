0858133c15183953317b50f545955aaf46e8c8dc
Muhammad Asadullah <muhammad.asad2020@gmail.com>
2021-04-20 16:31:32 -0700

    createChildRowfromParentRow

--- organogram/src/CourseController.js
+++ organogram/src/CourseController.js
@@ -1,276 +1,297 @@
 import React, { Component } from "react";
-
+import "./OrgChart.css";
 const coursePrerequisites = [
-  [
-    {
-      corequisites: [],
-      corequisitesExpanded: [],
-      credits: "3",
-      description: "",
-      number: "480",
-      prerequisites: ["FIN 303", "FIN 307", "FIN 345"],
-      prerequisitesExpanded: [
-        {
-          corequisites: [],
-          corequisitesExpanded: [],
-          credits: "3",
-          description: "",
-          number: "307",
-          prerequisites: ["FIN 301"],
-          prerequisitesExpanded: [
-            {
-              corequisites: [],
-              corequisitesExpanded: [],
-              credits: "3",
-              description: "",
-              number: "301",
-              prerequisites: ["ACC 201", "ACC 202", "HON 125"],
-              prerequisitesExpanded: [
-                {
-                  corequisites: [],
-                  corequisitesExpanded: [],
-                  credits: "3",
-                  description: "",
-                  number: "201",
-                  prerequisites: [],
-                  prerequisitesExpanded: [],
-                  subject: "ACC",
-                  title: "Financial Accounting",
-                },
-                {
-                  corequisites: [],
-                  corequisitesExpanded: [],
-                  credits: "3",
-                  description: "",
-                  number: "202",
-                  prerequisites: ["ACC 201"],
-                  prerequisitesExpanded: [
-                    {
-                      corequisites: [],
-                      corequisitesExpanded: [],
-                      credits: "3",
-                      description: "",
-                      number: "201",
-                      prerequisites: [],
-                      prerequisitesExpanded: [],
-                      subject: "ACC",
-                      title: "Financial Accounting",
-                    },
-                  ],
-                  subject: "ACC",
-                  title: "Managerial Accounting",
-                },
-                {
-                  corequisites: [],
-                  corequisitesExpanded: [],
-                  credits: "3",
-                  description: "",
-                  number: "125",
-                  prerequisites: ["HON 124"],
-                  prerequisitesExpanded: [
-                    {
-                      corequisites: [],
-                      corequisitesExpanded: [],
-                      credits: "3",
-                      description: "",
-                      number: "124",
-                      prerequisites: [],
-                      prerequisitesExpanded: [],
-                      subject: "HON",
-                      title: "Honors College Algebra",
-                    },
-                  ],
-                  subject: "HON",
-                  title: "Adventures in Data Analysis",
-                },
-              ],
-              subject: "FIN",
-              title: "Principles of Managerial Finance",
-            },
-          ],
-          subject: "FIN",
-          title: "Investments",
-        },
-        {
-          corequisites: [],
-          corequisitesExpanded: [],
-          credits: "3",
-          description: "",
-          number: "303",
-          prerequisites: ["FIN 301"],
-          prerequisitesExpanded: [
-            {
-              corequisites: [],
-              corequisitesExpanded: [],
-              credits: "3",
-              description: "",
-              number: "301",
-              prerequisites: ["ACC 201", "ACC 202", "HON 125"],
-              prerequisitesExpanded: [
-                {
-                  corequisites: [],
-                  corequisitesExpanded: [],
-                  credits: "3",
-                  description: "",
-                  number: "201",
-                  prerequisites: [],
-                  prerequisitesExpanded: [],
-                  subject: "ACC",
-                  title: "Financial Accounting",
-                },
-                {
-                  corequisites: [],
-                  corequisitesExpanded: [],
-                  credits: "3",
-                  description: "",
-                  number: "202",
-                  prerequisites: ["ACC 201"],
-                  prerequisitesExpanded: [
-                    {
-                      corequisites: [],
-                      corequisitesExpanded: [],
-                      credits: "3",
-                      description: "",
-                      number: "201",
-                      prerequisites: [],
-                      prerequisitesExpanded: [],
-                      subject: "ACC",
-                      title: "Financial Accounting",
-                    },
-                  ],
-                  subject: "ACC",
-                  title: "Managerial Accounting",
-                },
-                {
-                  corequisites: [],
-                  corequisitesExpanded: [],
-                  credits: "3",
-                  description: "",
-                  number: "125",
-                  prerequisites: ["HON 124"],
-                  prerequisitesExpanded: [
-                    {
-                      corequisites: [],
-                      corequisitesExpanded: [],
-                      credits: "3",
-                      description: "",
-                      number: "124",
-                      prerequisites: [],
-                      prerequisitesExpanded: [],
-                      subject: "HON",
-                      title: "Honors College Algebra",
-                    },
-                  ],
-                  subject: "HON",
-                  title: "Adventures in Data Analysis",
-                },
-              ],
-              subject: "FIN",
-              title: "Principles of Managerial Finance",
-            },
-          ],
-          subject: "FIN",
-          title: "Intermediate Managerial Finance",
-        },
-        {
-          corequisites: [],
-          corequisitesExpanded: [],
-          credits: "3",
-          description: "",
-          number: "345",
-          prerequisites: ["FIN 301"],
-          prerequisitesExpanded: [
-            {
-              corequisites: [],
-              corequisitesExpanded: [],
-              credits: "3",
-              description: "",
-              number: "301",
-              prerequisites: ["ACC 201", "ACC 202", "HON 125"],
-              prerequisitesExpanded: [
-                {
-                  corequisites: [],
-                  corequisitesExpanded: [],
-                  credits: "3",
-                  description: "",
-                  number: "201",
-                  prerequisites: [],
-                  prerequisitesExpanded: [],
-                  subject: "ACC",
-                  title: "Financial Accounting",
-                },
-                {
-                  corequisites: [],
-                  corequisitesExpanded: [],
-                  credits: "3",
-                  description: "",
-                  number: "202",
-                  prerequisites: ["ACC 201"],
-                  prerequisitesExpanded: [
-                    {
-                      corequisites: [],
-                      corequisitesExpanded: [],
-                      credits: "3",
-                      description: "",
-                      number: "201",
-                      prerequisites: [],
-                      prerequisitesExpanded: [],
-                      subject: "ACC",
-                      title: "Financial Accounting",
-                    },
-                  ],
-                  subject: "ACC",
-                  title: "Managerial Accounting",
-                },
-                {
-                  corequisites: [],
-                  corequisitesExpanded: [],
-                  credits: "3",
-                  description: "",
-                  number: "125",
-                  prerequisites: ["HON 124"],
-                  prerequisitesExpanded: [
-                    {
-                      corequisites: [],
-                      corequisitesExpanded: [],
-                      credits: "3",
-                      description: "",
-                      number: "124",
-                      prerequisites: [],
-                      prerequisitesExpanded: [],
-                      subject: "HON",
-                      title: "Honors College Algebra",
-                    },
-                  ],
-                  subject: "HON",
-                  title: "Adventures in Data Analysis",
-                },
-              ],
-              subject: "FIN",
-              title: "Principles of Managerial Finance",
-            },
-          ],
-          subject: "FIN",
-          title: "Managing New Venture Funding",
-        },
-      ],
-      subject: "FIN",
-      title: "Entrepreneurial Finance",
-    },
-  ],
+  {
+    corequisites: [],
+    corequisitesExpanded: [],
+    credits: "3",
+    description: "",
+    number: "480",
+    prerequisites: ["FIN 303", "FIN 307", "FIN 345"],
+    prerequisitesExpanded: [
+      {
+        corequisites: [],
+        corequisitesExpanded: [],
+        credits: "3",
+        description: "",
+        number: "307",
+        prerequisites: ["FIN 301"],
+        prerequisitesExpanded: [
+          {
+            corequisites: [],
+            corequisitesExpanded: [],
+            credits: "3",
+            description: "",
+            number: "301",
+            prerequisites: ["ACC 201", "ACC 202", "HON 125"],
+            prerequisitesExpanded: [
+              {
+                corequisites: [],
+                corequisitesExpanded: [],
+                credits: "3",
+                description: "",
+                number: "201",
+                prerequisites: [],
+                prerequisitesExpanded: [],
+                subject: "ACC",
+                title: "Financial Accounting",
+              },
+              {
+                corequisites: [],
+                corequisitesExpanded: [],
+                credits: "3",
+                description: "",
+                number: "202",
+                prerequisites: ["ACC 201"],
+                prerequisitesExpanded: [
+                  {
+                    corequisites: [],
+                    corequisitesExpanded: [],
+                    credits: "3",
+                    description: "",
+                    number: "201",
+                    prerequisites: [],
+                    prerequisitesExpanded: [],
+                    subject: "ACC",
+                    title: "Financial Accounting",
+                  },
+                ],
+                subject: "ACC",
+                title: "Managerial Accounting",
+              },
+              {
+                corequisites: [],
+                corequisitesExpanded: [],
+                credits: "3",
+                description: "",
+                number: "125",
+                prerequisites: ["HON 124"],
+                prerequisitesExpanded: [
+                  {
+                    corequisites: [],
+                    corequisitesExpanded: [],
+                    credits: "3",
+                    description: "",
+                    number: "124",
+                    prerequisites: [],
+                    prerequisitesExpanded: [],
+                    subject: "HON",
+                    title: "Honors College Algebra",
+                  },
+                ],
+                subject: "HON",
+                title: "Adventures in Data Analysis",
+              },
+            ],
+            subject: "FIN",
+            title: "Principles of Managerial Finance",
+          },
+        ],
+        subject: "FIN",
+        title: "Investments",
+      },
+      {
+        corequisites: [],
+        corequisitesExpanded: [],
+        credits: "3",
+        description: "",
+        number: "303",
+        prerequisites: ["FIN 301"],
+        prerequisitesExpanded: [
+          {
+            corequisites: [],
+            corequisitesExpanded: [],
+            credits: "3",
+            description: "",
+            number: "301",
+            prerequisites: ["ACC 201", "ACC 202", "HON 125"],
+            prerequisitesExpanded: [
+              {
+                corequisites: [],
+                corequisitesExpanded: [],
+                credits: "3",
+                description: "",
+                number: "201",
+                prerequisites: [],
+                prerequisitesExpanded: [],
+                subject: "ACC",
+                title: "Financial Accounting",
+              },
+              {
+                corequisites: [],
+                corequisitesExpanded: [],
+                credits: "3",
+                description: "",
+                number: "202",
+                prerequisites: ["ACC 201"],
+                prerequisitesExpanded: [
+                  {
+                    corequisites: [],
+                    corequisitesExpanded: [],
+                    credits: "3",
+                    description: "",
+                    number: "201",
+                    prerequisites: [],
+                    prerequisitesExpanded: [],
+                    subject: "ACC",
+                    title: "Financial Accounting",
+                  },
+                ],
+                subject: "ACC",
+                title: "Managerial Accounting",
+              },
+              {
+                corequisites: [],
+                corequisitesExpanded: [],
+                credits: "3",
+                description: "",
+                number: "125",
+                prerequisites: ["HON 124"],
+                prerequisitesExpanded: [
+                  {
+                    corequisites: [],
+                    corequisitesExpanded: [],
+                    credits: "3",
+                    description: "",
+                    number: "124",
+                    prerequisites: [],
+                    prerequisitesExpanded: [],
+                    subject: "HON",
+                    title: "Honors College Algebra",
+                  },
+                ],
+                subject: "HON",
+                title: "Adventures in Data Analysis",
+              },
+            ],
+            subject: "FIN",
+            title: "Principles of Managerial Finance",
+          },
+        ],
+        subject: "FIN",
+        title: "Intermediate Managerial Finance",
+      },
+      {
+        corequisites: [],
+        corequisitesExpanded: [],
+        credits: "3",
+        description: "",
+        number: "345",
+        prerequisites: ["FIN 301"],
+        prerequisitesExpanded: [
+          {
+            corequisites: [],
+            corequisitesExpanded: [],
+            credits: "3",
+            description: "",
+            number: "301",
+            prerequisites: ["ACC 201", "ACC 202", "HON 125"],
+            prerequisitesExpanded: [
+              {
+                corequisites: [],
+                corequisitesExpanded: [],
+                credits: "3",
+                description: "",
+                number: "201",
+                prerequisites: [],
+                prerequisitesExpanded: [],
+                subject: "ACC",
+                title: "Financial Accounting",
+              },
+              {
+                corequisites: [],
+                corequisitesExpanded: [],
+                credits: "3",
+                description: "",
+                number: "202",
+                prerequisites: ["ACC 201"],
+                prerequisitesExpanded: [
+                  {
+                    corequisites: [],
+                    corequisitesExpanded: [],
+                    credits: "3",
+                    description: "",
+                    number: "201",
+                    prerequisites: [],
+                    prerequisitesExpanded: [],
+                    subject: "ACC",
+                    title: "Financial Accounting",
+                  },
+                ],
+                subject: "ACC",
+                title: "Managerial Accounting",
+              },
+              {
+                corequisites: [],
+                corequisitesExpanded: [],
+                credits: "3",
+                description: "",
+                number: "125",
+                prerequisites: ["HON 124"],
+                prerequisitesExpanded: [
+                  {
+                    corequisites: [],
+                    corequisitesExpanded: [],
+                    credits: "3",
+                    description: "",
+                    number: "124",
+                    prerequisites: [],
+                    prerequisitesExpanded: [],
+                    subject: "HON",
+                    title: "Honors College Algebra",
+                  },
+                ],
+                subject: "HON",
+                title: "Adventures in Data Analysis",
+              },
+            ],
+            subject: "FIN",
+            title: "Principles of Managerial Finance",
+          },
+        ],
+        subject: "FIN",
+        title: "Managing New Venture Funding",
+      },
+    ],
+    subject: "FIN",
+    title: "Entrepreneurial Finance",
+  },
 ];
 
-const convertPrerequisiteTreeTorows = (courses) => {
-  const row1 = courses[0];
-  
-  console.log("convertPrerequisiteTreeTorows");
-  console.log("row1" + row1);
-  return [row1];
+const flattern = (arr) => {
+  return arr.reduce(function (a, b) {
+    return a.concat(b);
+  }, []);
+};
+
+const convertPrerequisiteTreeTorows = (Parentcourse) => {
+  const createChildRowfromParentRow = (parentRow) => {
+    const row = flattern(
+      parentRow.map((rowCourses) => rowCourses["prerequisitesExpanded"])
+    );
+    if (row.find((course) => course.prerequisitesExpanded.length > 0)) {
+      return [row].concat(createChildRowfromParentRow(row));
+    }
+    return row;
+  };
+
+  //debugger;
+  const row1 = [Parentcourse];
+  //  console.log('Diff between Parentcourse and [Parentcourse]')
+  //   console.log(Parentcourse)
+  //   console.log([Parentcourse])
+  //const row2 = Parentcourse["prerequisitesExpanded"];
+   const rows = [row1].concat(createChildRowfromParentRow(row1))
+  //   console.log("convertPrerequisiteTreeTorows");
+  //   console.log("row1" + row1);
+ // return [row1, row2];
   // return [
   //     ["FIN 408"],
   //     ["FIN 307","FIN 303","FIN 305"],
   //     ["FIN 201","FIN 202"]
   // ]
+
+  return rows
 };
 const OrgChartNode = ({ course }) => {
   return (
@@ -283,14 +304,15 @@ const OrgChartNode = ({ course }) => {
   );
 };
 const OrgChart = ({ rows }) => {
-  const rowNodes = rows.map((row) =>
+  const rowNodes = rows.map((row) => (
     // console.log(rows)
-
-    row.map((course) => (
-      <OrgChartNode course={course} />
-      //console.log(course)
-    ))
-  );
+    <div>
+      {row.map((course) => (
+        <OrgChartNode course={course} />
+        //console.log(course)
+      ))}
+    </div>
+  ));
 
   return (
     <div>
@@ -307,7 +329,8 @@ class CourseController extends Component {
   }
   componentDidMount() {}
   render() {
-    const rows = convertPrerequisiteTreeTorows(coursePrerequisites);
+    debugger;
+    const rows = convertPrerequisiteTreeTorows(coursePrerequisites[0]);
     console.log(rows);
     return (
       <div>
